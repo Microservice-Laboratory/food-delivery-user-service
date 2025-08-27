6742b82bf1d4faa04d12c381094088146b5d152e
leandrosilva-dev <android.leandro.siqueira@gmail.com>
2025-08-27 13:05:20 -0300

    feat: force smell code

--- internal/api/healthcheck/handlers.go
+++ internal/api/healthcheck/handlers.go
@@ -7,14 +7,6 @@ import (
 )
 
 func HealthCheckHandler(c *gin.Context) {
-	if 1 == 3 {
-
-	} else {
-		if 1 == 2 {
-
-		}
-	}
-
 	c.Header("Cache-Control", "no-store")
 	c.JSON(http.StatusOK, gin.H{"status": "UP"})
 }
