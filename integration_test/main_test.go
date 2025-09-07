54a9e049a5afa3836ab1f8cd8cc3a0486cb9d9f2
leandrosilva-dev <android.leandro.siqueira@gmail.com>
2025-09-07 12:42:13 -0300

    fix: create a temporary test container for Postgres

--- integration_test/main_test.go
+++ integration_test/main_test.go
@@ -1,12 +1,18 @@
 package integrationtest_test
 
 import (
+	"context"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 
 	"Microservice-Laboratory/food-delivery-user-service/internal/api"
 )
@@ -15,14 +21,49 @@ func TestHealthCheckEndpoi(t *testing.T) {
 	// Arrange
 	gin.SetMode(gin.TestMode)
 
-	router := api.SetupRouter()
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "postgres:latest",
+		ExposedPorts: []string{"5432/tcp"},
+		WaitingFor:   wait.ForLog("database system is ready to accept connections"),
+		Env: map[string]string{
+			"POSTGRES_DB":       "test",
+			"POSTGRES_USER":     "test",
+			"POSTGRES_PASSWORD": "test",
+		},
+	}
+
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	require.NoError(t, err)
+
+	// Ensure the container is cleaned up after the test finishes
+	defer postgresC.Terminate(ctx)
+
+	host, err := postgresC.Host(ctx)
+	require.NoError(t, err)
+	port, err := postgresC.MappedPort(ctx, "5432/tcp")
+	require.NoError(t, err)
+
+	// Construct the connection string
+	connStr := fmt.Sprintf("host=%s port=%s user=test password=test dbname=test sslmode=disable", host, port.Port())
+
+	// Open the database connection
+	db, err := sql.Open("pgx", connStr)
+	require.NoError(t, err)
+	defer db.Close()
+
+	router := api.SetupRouter(db)
 
 	w := httptest.NewRecorder()
 
-	req, _ := http.NewRequest(http.MethodGet, "/api/v1/health", nil)
+	request, _ := http.NewRequest(http.MethodGet, "/api/v1/health", nil)
 
 	//Act
-	router.ServeHTTP(w, req)
+	router.ServeHTTP(w, request)
 
 	// Assert
 	if w.Code != http.StatusOK {
@@ -35,13 +76,15 @@ func TestHealthCheckEndpoi(t *testing.T) {
 	}
 
 	var response map[string]string
-	err := json.Unmarshal(w.Body.Bytes(), &response)
-	if err != nil {
-		t.Fatalf("Failed to unmarshal response body: %v", err)
+	errUnmarshal := json.Unmarshal(w.Body.Bytes(), &response)
+	if errUnmarshal != nil {
+		t.Fatalf("Failed to unmarshal response body: %v", errUnmarshal)
 	}
 
 	expectedStatus := "UP"
 	if response["status"] != expectedStatus {
 		t.Errorf("Expected status 'UP', got %s", response["status"])
 	}
+
+	defer postgresC.Terminate(ctx)
 }
